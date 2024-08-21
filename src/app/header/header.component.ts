b47d7b2aa95584a4fae9532f5669e2f2b5ac691b
HARVEYNASH\tuong.trankien <tuong.trankien@nashtechglobal.com>
2024-08-21 20:01:55 +0700

    Handle & display items in cart

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,25 +4,27 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import {MatListModule} from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe, CommonModule, CurrencyPipe } from '@angular/common';
 import { CartService } from '../services/cart.service';
 import { ProductCart } from '../services/product.model';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, RouterLink, RouterLinkActive, NgIf, MatListModule, AsyncPipe],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule, RouterLink, RouterLinkActive, CommonModule, MatListModule, AsyncPipe, CurrencyPipe, FlexLayoutModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit {
   productsInCart: ProductCart[] = [];
   totalProductsInCart: number = 0;
-  destroyRef = inject(DestroyRef);
+  private destroyRef = inject(DestroyRef);
   // userName = computed(() => this.authService.currentUser.value.userName);
+  totalCost?: number;
 
   constructor(public authService: AuthService,private cartService: CartService) {}
 
@@ -34,6 +36,7 @@ export class HeaderComponent implements OnInit {
     const cartSubscription = this.cartService.productsInCart$.subscribe((products) => {
       this.productsInCart = products;
       this.totalProductsInCart = products.length;
+      this.totalCost = this.productsInCart.reduce((sum, item) => sum + item.price * item.quantity, 0);
     });
 
     this.destroyRef.onDestroy(() => {
